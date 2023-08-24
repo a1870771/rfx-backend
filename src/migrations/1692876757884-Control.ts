e5caf35752a9747e572d96763b13d818130ff698
Jack <jackrlarkin2004@gmail.com>
2023-08-24 22:40:00 +0930

    fffff

---
+++ src/migrations/1692876757884-Control.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class Control1692876757884 implements MigrationInterface {
+
+public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+        `CREATE TABLE IF NOT EXISTS "post" (
+            "id" character varying NOT NULL,
+            "name" character varying NOT NULL,
+            "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+            "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+        )`
+    )
+    await queryRunner.createPrimaryKey("post", ["id"])
+}
+
+public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("post", true)
+}
+
+
+}
---
+++ src/models/product.ts
@@ -0,0 +1,12 @@
+import { Column, Entity } from "typeorm"
+import {
+  // alias the core entity to not cause a naming conflict
+  Product as MedusaProduct,
+} from "@medusajs/medusa"
+
+@Entity()
+export class Product extends MedusaProduct {
+  @Column()
+  controlName: string
+  controlDesc: string
+}
